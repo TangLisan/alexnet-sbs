224572c8edd4f961d770af5ce1fe0ec52a30d4a7
Frank ZHou <zhiyan.zhou@stonybrookschool.org>
2018-07-08 18:10:17 -0400

    Add functions for data preprocessing

--- src/chest_xray_tf/data_preprocessing_hf.py
+++ src/chest_xray_tf/data_preprocessing_hf.py
@@ -1,1 +1,65 @@
 # The functions that help pre-process(Not fetching) data should be put in here
+import numpy as np
+import tensorflow as tf
+from fetch_hf import get_image_data
+import matplotlib.pyplot as plt
+
+
+def largest_image_size(img_array):
+    max_row = float('-inf')
+    max_column = float('-inf')
+    for i in range(1, len(img_array), 2):
+        array = img_array[i]
+        if len(array) > max_row:
+            max_row = len(array)
+        if len(array[0]) > max_column:
+            max_column = len(array)
+    largest_size = (max_row, max_column)
+    return largest_size
+
+
+def smallest_image_size(img_array):
+    min_row = float('inf')
+    min_column = float('inf')
+    for i in range(1, len(img_array), 2):
+        array = img_array[i]
+        if len(array) < min_row:
+            min_row = len(array)
+        if len(array[0]) < min_column:
+            min_column = len(array)
+    largest_size = (min_row, min_column)
+    return largest_size
+
+
+def resize_img_numpy(img_list, shape):
+    result = []
+    print("Start resizing images...")
+    for img in img_list:
+        img = tf.constant(img)
+        img = tf.image.resize_images(img, shape)
+        result.append(img)
+    print("Done.")
+    return result
+
+
+#The following codes test the function above
+# label_list,img_list = get_image_data("/Users/frank/Documents/GitHub/alexnet-sbs/dataSet/train/NORMAL")
+#
+# size = tf.constant([100, 100])
+#
+# test = resize_img_numpy(img_list[:10], size)[3]
+#
+# sess = tf.InteractiveSession()
+# im = sess.run(test)
+# row = im.shape[0]
+# column = im.shape[1]
+# im = im.flatten().reshape(row, column)
+#
+#
+#  with tf.Session() as sess:
+#      print(sess.run(test))
+#
+# plt.imshow(im[::], cmap="gray")
+# plt.show()
+
+
--- src/chest_xray_tf/fetch_hf.py
+++ src/chest_xray_tf/fetch_hf.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import cv2
+from numpy import newaxis
 # All the function that will be used to fetch unprocessed data should be put here
 
 
@@ -8,7 +9,8 @@ import cv2
 # Return: an numpy array consisting 1. the label of the image
 #                                   2. the numpy array version of the image
 def get_image_data(folder_address):
-    result_list = []
+    label_list = []
+    image_list = []
     directory = os.fsencode(folder_address)
     for file in os.listdir(directory):  # Iterate through the folder
         filename = os.fsdecode(file)  # File name in string
@@ -16,15 +18,19 @@ def get_image_data(folder_address):
         if filename.endswith(".jpeg"):
             img_path = folder_address + "/" + filename
             img_array = cv2.imread(img_path, 0)  # Converting image into numpy array
+            img_array = np.reshape(img_array, img_array.shape + (1,))
             if filename.find("bacteria") != -1:
-                result_list.append("bacteria")
-                result_list.append(img_array)
+                label_list.append("bacteria")
+                image_list.append(img_array)
             elif filename.find("virus") != -1:
-                result_list.append("virus")
-                result_list.append(img_array)
+                label_list.append("virus")
+                image_list.append(img_array)
             else:
-                result_list.append("normal")
-                result_list.append(img_array)
+                label_list.append("normal")
+                image_list.append(img_array)
         print("Done.")
-    result_list = np.array(result_list, dtype=object)
-    return result_list
+    image_list = np.array(image_list, dtype=object)
+    label_list = np.array(label_list, dtype=object)
+    return label_list, image_list
+
+
