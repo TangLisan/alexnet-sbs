753b127fdd9c242ec8946f66f2ecb54e88375e02
TomGao2001 <gzm2001@outlook.com>
2018-07-11 21:31:29 -0700

    Network goal clarification

--- src/chest_xray_tf/layer.py
+++ src/chest_xray_tf/layer.py
@@ -1,2 +1,7 @@
 from layer_hf import convolutional_layer
 from layer_hf import normal_full_layer
+
+#X is a reshaped 4D tensor with size [batch (param), height (param), width (param), channel = 1]
+#Output one-hot label: list (norma, bacteria, virus)
+def sbs_alex_net(X, W, b, dropout):
+	conv1 = convolutional_layer(X, shape = [])
